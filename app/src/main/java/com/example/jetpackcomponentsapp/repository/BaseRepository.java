99850aa3101f3b9b042374707d44a58c21c770f8
Benedict <benedictcontawe@gmail.com>
2020-01-05 12:13:30 +0800

    update BaseRepository

--- app/src/main/java/com/example/jetpackcomponentsapp/repository/BaseRepository.java
+++ app/src/main/java/com/example/jetpackcomponentsapp/repository/BaseRepository.java
@@ -1,4 +1,14 @@
 package com.example.jetpackcomponentsapp.repository;
 
+import com.example.jetpackcomponentsapp.model.CustomModel;
+
 public interface BaseRepository {
+
+    void insert(CustomModel customModel);
+
+    void update(CustomModel customModel);
+
+    void delete(CustomModel customModel);
+
+    void deleteAll();
 }
