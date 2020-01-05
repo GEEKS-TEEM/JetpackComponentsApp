924fd7f3f75ff8c800d3b17e911952a299cb064e
Benedict <benedictcontawe@gmail.com>
2020-01-05 12:52:12 +0800

    Update Main Repository and Room Data Access Object

--- app/src/main/java/com/example/jetpackcomponentsapp/repository/CustomRepository.java
+++ app/src/main/java/com/example/jetpackcomponentsapp/repository/CustomRepository.java
@@ -1,8 +1,32 @@
 package com.example.jetpackcomponentsapp.repository;
 
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
 import com.example.jetpackcomponentsapp.model.CustomModel;
+import com.example.jetpackcomponentsapp.room.CustomDAO;
+
+import java.util.List;
 
 public class CustomRepository implements BaseRepository {
+
+    private CustomDAO customDAO;
+
+    private static CustomRepository INSTANCE;
+
+    public static CustomRepository getInstance(Application applicationContext) {
+        if (INSTANCE == null) {
+            return new CustomRepository(applicationContext);
+        }
+        else {
+            return INSTANCE;
+        }
+    }
+
+    private CustomRepository(Application applicationContext) {
+
+    }
 
     @Override
     public void insert(CustomModel customModel) {
@@ -22,5 +46,11 @@ public class CustomRepository implements BaseRepository {
     @Override
     public void deleteAll() {
 
+    }
+
+    public LiveData<List<CustomModel>> getAll() {
+        return ConvertList.toLiveDataListModel(
+                customDAO.getAll()
+        );
     }
 }
