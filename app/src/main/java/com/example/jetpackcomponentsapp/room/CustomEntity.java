806eb0bf3543ab2f8f4fc967970251e32a2c07bf
Benedict <benedictcontawe@gmail.com>
2020-01-05 12:28:48 +0800

    room Entity updated

--- app/src/main/java/com/example/jetpackcomponentsapp/room/CustomEntity.java
+++ app/src/main/java/com/example/jetpackcomponentsapp/room/CustomEntity.java
@@ -1,7 +1,42 @@
 package com.example.jetpackcomponentsapp.room;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "custom_table")
 public class CustomEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "Id")
+    private int id;
+
+    @ColumnInfo(name = "Name")
+    private String name;
+
+    @ColumnInfo(name = "Icon")
+    private int icon;
+
+    public CustomEntity(String name,int icon) {
+        this.name = name;
+        this.icon = icon;
+    }
+
+    public CustomEntity(int id, String name,int icon) {
+        this.id = id;
+        this.name = name;
+        this.icon = icon;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
 }
