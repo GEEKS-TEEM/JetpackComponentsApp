567df848dc39bde6a1f4b3325c84c988056763b1
Benedict <benedictcontawe@gmail.com>
2020-01-05 12:30:33 +0800

    Main Repository update, under construction

--- app/src/main/java/com/example/jetpackcomponentsapp/repository/CustomRepository.java
+++ app/src/main/java/com/example/jetpackcomponentsapp/repository/CustomRepository.java
@@ -1,4 +1,26 @@
 package com.example.jetpackcomponentsapp.repository;
 
+import com.example.jetpackcomponentsapp.model.CustomModel;
+
 public class CustomRepository implements BaseRepository {
+
+    @Override
+    public void insert(CustomModel customModel) {
+
+    }
+
+    @Override
+    public void update(CustomModel customModel) {
+
+    }
+
+    @Override
+    public void delete(CustomModel customModel) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
