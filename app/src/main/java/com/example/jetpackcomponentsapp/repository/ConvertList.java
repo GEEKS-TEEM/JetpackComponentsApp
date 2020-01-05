27def4d3601f277b2b54ca14aaf78878bc43c068
Benedict <benedictcontawe@gmail.com>
2020-01-05 12:29:16 +0800

    Repository converter updated but under construction

--- app/src/main/java/com/example/jetpackcomponentsapp/repository/ConvertList.java
+++ app/src/main/java/com/example/jetpackcomponentsapp/repository/ConvertList.java
@@ -1,4 +1,46 @@
 package com.example.jetpackcomponentsapp.repository;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
+
+import com.example.jetpackcomponentsapp.model.CustomModel;
+import com.example.jetpackcomponentsapp.room.CustomEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class ConvertList {
+
+    public static List<CustomModel> toListModel(List<CustomEntity> customEntity) {
+        List<CustomModel> itemList = new ArrayList<>();
+
+        for (CustomEntity itemMap : customEntity) {
+            itemList.add(
+                    new CustomModel(itemMap.getId(),itemMap.getName())
+            );
+        }
+
+        return itemList;
+    }
+
+    /*public static LiveData<List<CustomModel>> toLiveDataListModel(LiveData<List<CustomEntity>> localList ) {
+
+    }*/
+
+    /*public static CustomEntity  toEntity(CustomModel customModel) {
+        if (customModel.getId() == null) {
+            return new CustomEntity(
+                    customModel.getName(),
+                    customModel.getIcon()
+            );
+        }
+        else {
+            return new CustomEntity(
+                    customModel.getId(),
+                    customModel.getName(),
+                    customModel.getIcon()
+            );
+        }
+    }*/
 }
