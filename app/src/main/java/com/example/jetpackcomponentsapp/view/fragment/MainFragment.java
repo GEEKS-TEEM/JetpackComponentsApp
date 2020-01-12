52c1d0f388752b86dafe2800b536b54e94081912
Benedict <benedictcontawe@gmail.com>
2020-01-12 14:01:38 +0800

    deleting toast. Replaced by CRUD

--- app/src/main/java/com/example/jetpackcomponentsapp/view/fragment/MainFragment.java
+++ app/src/main/java/com/example/jetpackcomponentsapp/view/fragment/MainFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -84,7 +83,6 @@ public class MainFragment extends Fragment implements CustomListeners {
             @Override
             public void onClick(View view) {
                 viewModel.deleteAll();
-                Toast.makeText(getContext(),"deleteAll()", Toast.LENGTH_SHORT).show();
             }
         });
     }
